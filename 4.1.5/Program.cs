076c6dd9dcd6d8d434c7f41539039443b30e9884
Dmitry <ryabchikk@rambler.ru>
2022-09-22 21:51:31 +0300

    4.1.5

---
+++ 4.1.5/Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int a;
+        int b;
+        double x;
+        double y;
+
+        bool z = (a < b) | (x > y);
+
+    }
+}
