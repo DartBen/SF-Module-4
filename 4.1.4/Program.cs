3e533946716d777f634615a79731db7ec2a7630a
Dmitry <ryabchikk@rambler.ru>
2022-09-22 21:46:45 +0300

    4.1.4

---
+++ 4.1.4/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string a = "Aa";
+        string b = "Bb";
+        bool test = (a==b);
+        Console.WriteLine($"результат {a}=={b} = {test}");
+
+        a = "Aa";
+        b = "Aa";
+        test = (a == b);
+        Console.WriteLine($"результат {a}=={b} = {test}");
+        Console.ReadKey();
+
+    }
+}
