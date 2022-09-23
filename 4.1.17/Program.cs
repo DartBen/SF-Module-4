a500c82a49ad987f91806d45120fcf3743b2e606
DartBen <ryabchikk@rambler.ru>
2022-09-23 10:25:23 +0300

    merge 4.1.17
    Signed-off-by: DartBen <ryabchikk@rambler.ru>

--- 4.1.17/Program.cs
+++ 4.1.17/Program.cs
@@ -3,5 +3,39 @@
     private static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+	    Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+
+		var color = Console.ReadLine();
+
+		switch(color)
+		{
+			case "red":
+				Console.BackgroundColor = ConsoleColor.Red;
+				Console.ForegroundColor = ConsoleColor.Black;
+
+				Console.WriteLine("Your color is red!");
+			break;
+
+			case "green":
+				Console.BackgroundColor = ConsoleColor.Green;
+				Console.ForegroundColor = ConsoleColor.Black;
+
+				Console.WriteLine("Your color is green!");
+			break;
+
+            case "cian":
+				Console.BackgroundColor = ConsoleColor.Cyan;
+				Console.ForegroundColor = ConsoleColor.Black;
+
+				Console.WriteLine("Your color is cyan!");
+			break;
+
+			default:
+				Console.BackgroundColor = ConsoleColor.Yellow;
+				Console.ForegroundColor = ConsoleColor.Red;
+
+				Console.WriteLine("Your color is cyan!");
+			break;
+		}
     }
 }
