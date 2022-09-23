59a0d5e3b33f916bd38623cb4ae0b67026a865f1
DartBen <ryabchikk@rambler.ru>
2022-09-23 22:23:31 +0300

    4.3.7

---
+++ 4.3.7/Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введите своё имя");
+
+        var name = Console.ReadLine();
+
+        string nameResult="";
+
+        Console.WriteLine("Ваше имя по буквам: ");
+    
+        for(int i= name.Length-1;i>=0 ;i--)
+        {
+            nameResult = string.Concat(nameResult, name[i]);
+
+        }
+        Console.WriteLine(nameResult);
+
+    }
+}
