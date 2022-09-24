0f4b244a6e73fbdc040b93f1606f8db1ccc568a0
DartBen <ryabchikk@rambler.ru>
2022-09-24 14:48:39 +0300

    4.4.5

---
+++ 4.4.5/Program.cs
@@ -0,0 +1,16 @@
+﻿using static System.Console;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        (string Name, string Type, double Age, int NameCount) Pet;
+
+        WriteLine("Введите имя питомца:");
+        Pet.Name = ReadLine();
+        WriteLine("Введите вид питомца:");
+        Pet.Type = ReadLine();
+        WriteLine("Введите возраст питомца:");
+        double.TryParse(ReadLine(), out Pet.Age);
+        Pet.NameCount = Pet.Name.Length;
+    }
+}
