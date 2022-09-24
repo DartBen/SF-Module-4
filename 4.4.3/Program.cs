efac4fe3fbf65d61e7c6f76a77d7548016a3ed1f
DartBen <ryabchikk@rambler.ru>
2022-09-24 14:44:47 +0300

    4.4.4

---
+++ 4.4.3/Program.cs
@@ -0,0 +1,22 @@
+﻿using static System.Console;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var (name, age) = ("Дмитрий", 32);
+        Console.WriteLine("Ваше имя: {0}", name);
+        Console.WriteLine("Ваш возраст: {0}", age);
+
+
+        //var anketa = (name: "", age: 0);
+
+        WriteLine("Введите имя:");
+        name = ReadLine();
+        WriteLine("Введите возраст:");
+        int.TryParse(ReadLine(), out age);
+
+        Console.WriteLine("Ваше имя: {0}", name);
+        Console.WriteLine("Ваш возраст: {0}", age);
+        Console.ReadKey();
+    }
+}
---
+++ 4.4.4/Program.cs
@@ -0,0 +1,10 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+     
+        var Pet=(Name:"",Age:0,Type:"");
+        (string Name, double Age, string Type) Pet2;
+
+    }
+}
