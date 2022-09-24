7bf2336c296b90a1baba2a15836adf03ec3e5bfa
DartBen <ryabchikk@rambler.ru>
2022-09-24 13:50:17 +0300

    4.3.17

---
+++ 4.3.16/Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[,] arr = { { -5, 6, 9, 1, 2, -3 }, { -8, 8, 1, 1, 2, -3 } };
+
+        int result = 0;
+
+        foreach (int i in arr)
+        {
+            if (i > 0)
+                result++;
+        }
+        Console.WriteLine(result);
+    }
+}
---
+++ 4.3.17/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[,] arr = { { -5, 6, 9, 1, 2, -3 }, { -8, 8, 1, 1, 2, -3 } };
+
+
+        int temp;
+
+        int a = arr.GetUpperBound(0);
+        int b = arr.GetUpperBound(1);
+
+
+        for (int y = 0; y < a+1; y++)
+        {
+            for (int z= 0; z < b; z++)
+            {
+
+                for (int i = 0; i < b; i++)
+                {
+
+                    if (arr[y,i] > arr[y,i + 1])
+                    {
+                        temp = arr[y,i];
+                        arr[y,i] = arr[y,i + 1];
+                        arr[y,i + 1] = temp;
+                    };
+
+                }
+            }
+        }
+
+        foreach(int t in arr) Console.WriteLine(t);
+
+    }
+}
