3eb8b05f9dfd68d5e693b21956af8b13fea0ef62
DartBen <ryabchikk@rambler.ru>
2022-09-24 14:26:58 +0300

    4.4.2

---
+++ 4.4.2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Net.Cache;
+using static System.Console;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var anketa = (name:"",age:0);
+
+        WriteLine("Введите имя:");
+        anketa.name=ReadLine();
+        WriteLine("Введите возраст:");
+        int.TryParse( ReadLine(),out anketa.age);
+
+        Console.WriteLine("Ваше имя: {0}", anketa.Item1);
+        Console.WriteLine("Ваш возраст: {0}", anketa.Item2);
+        Console.ReadKey();
+    }
+}
+
+enum DaysOfWeek : byte
+{
+    Monday = 1,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday
+}
