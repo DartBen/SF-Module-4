8e3c79d091135953f16e1348afafbaf002883069
DartBen <ryabchikk@rambler.ru>
2022-09-24 13:26:48 +0300

    4.3.15

---
+++ 4.3.15/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        Array arr = new int[2] { 1, 2 };
+        int result = 0;
+
+        foreach (int i in arr)
+        {
+            if (i > 0)
+                result++; 
+
+        }
+        Console.WriteLine(result);
+    }
+}
